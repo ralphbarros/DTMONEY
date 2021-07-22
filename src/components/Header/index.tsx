3c2b4c843242f1cf3d09af5895d17fd5795b598e
ralphbarros <ralphbarros@gmail.com>
2021-07-22 08:51:48 -0300

    Created Header Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,16 @@
 import logoImg from '../../assets/logo.svg'
-import { Container } from './style'
+import { Container, Content } from './style'
 
 
 export function Header(){
     return(
         <Container>
-            <img src={logoImg} alt="dt monet" />
-            <button type="button">
-                Nova Transação
-            </button>
+            <Content>
+                <img src={logoImg} alt="dt monet" />
+                <button type="button">
+                    Nova Transação
+                </button>
+            </Content>
         </Container>
 
 
